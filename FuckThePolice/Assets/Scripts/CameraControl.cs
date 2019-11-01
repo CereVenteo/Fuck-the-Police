e08def40caa03985d283cfb791fcf908607f2340
CereVenteo <36203330+CereVenteo@users.noreply.github.com>
2019-11-01 23:35:03 +0100

    camera movement

---
+++ FuckThePolice/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+
+    private Vector3 cameraFollowPosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveAmount = 10f;
+        float edgeSize = 30f;
+        cameraFollowPosition = transform.position;
+
+        if (Input.mousePosition.x > Screen.width - edgeSize)
+        {
+            cameraFollowPosition.x += moveAmount * Time.deltaTime;
+        }
+        if (Input.mousePosition.x < edgeSize)
+        {
+            cameraFollowPosition.x -= moveAmount * Time.deltaTime;
+        }
+        if (Input.mousePosition.y > Screen.height - edgeSize)
+        {
+            cameraFollowPosition.y += moveAmount * Time.deltaTime;
+        }
+        if (Input.mousePosition.y <  edgeSize)
+        {
+            cameraFollowPosition.y -= moveAmount * Time.deltaTime;
+        }
+
+        transform.position = cameraFollowPosition;
+    }
+}
