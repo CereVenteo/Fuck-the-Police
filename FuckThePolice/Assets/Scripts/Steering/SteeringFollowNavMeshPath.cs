ca13aaaaa2caf383052fd195da1e8f59dd5a6e04
Alejandro-PC\Alejandro <>
2019-11-03 09:43:46 +0100

    follow bgcurve path

--- FuckThePolice/Assets/Scripts/Steering/SteeringFollowNavMeshPath.cs
+++ FuckThePolice/Assets/Scripts/Steering/SteeringFollowNavMeshPath.cs
@@ -2,44 +2,42 @@
 using System.Collections;
 using BansheeGz.BGSpline.Components;
 using BansheeGz.BGSpline.Curve;
+using UnityEngine.AI;
 
-public class SteeringFollowPath : SteeringAbstract
+// TO FOLLOW PATH
+public class SteeringFollowNavMeshPath : SteeringAbstract
 {
     Move move;
+    public PathManager man;
     SteeringSeek seek;
-    public BGCcMath path;
+    SteeringArrive arrive;
     Vector3 closest_point;
 
     public float ratio_increment = 0.1f;
-    public float min_distance = 1.0f;
+    public float min_distance = 0.5f;
     float current_ratio = 0.0f;
 
     // Use this for initialization
     void Start()
     {
+        //man = GameObject.Find("CurveManager").GetComponent<PathManager>();
         move = GetComponent<Move>();
         seek = GetComponent<SteeringSeek>();
+        arrive = GetComponent<SteeringArrive>();
 
         // TODO 1: Calculate the closest point from the tank to the curve
-        float distance;
-        closest_point = path.CalcPositionByClosestPoint(transform.position, out distance);
-        current_ratio = distance / path.Curve.Points.Length;
+        //float distance;
+        //current_ratio = distance / man.path.Curve.Points.Length;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if (Vector3.Distance(transform.position, closest_point) <= min_distance)
-        {
-            current_ratio += ratio_increment;
-            if (current_ratio > 1)
-                current_ratio = 0;
-            closest_point = path.CalcPositionByDistanceRatio(current_ratio);
-        }
-
-        seek.Steer(closest_point);
-        // TODO 2: Check if the tank is close enough to the desired point
-        // If so, create a new point further ahead in the path
+    { 
+        closest_point = GetComponent<NavMeshAgent>().path.corners[1];
+        if (GetComponent<NavMeshAgent>().path.corners.Length > 2)
+            seek.Steer(closest_point);
+        else
+            arrive.Steer(closest_point);
     }
 
     void OnDrawGizmosSelected()
