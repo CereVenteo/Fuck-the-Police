997775d2b9cfed0a9cb68ae76f2f1fa0297288d8
CereVenteo <36203330+CereVenteo@users.noreply.github.com>
2019-11-27 19:07:31 +0100

    creating UI
    code time
    code points
    code stars
    code turn number

--- FuckThePolice/Assets/Scripts/Canvas.cs
+++ FuckThePolice/Assets/Scripts/Canvas.cs
@@ -1,18 +1,101 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Canvas : MonoBehaviour
 {
+    public Text time;
+    public Text day;
+    uint time_valor;
+    uint hour;
+    uint day_valor;
+
+    public Text points;
+    public Text next_star;
+    uint star_points;
+    uint points_valor;
+
+    public Image star1;
+    public Image star2;
+    public Image star3;
+    public Image star4;
+    public Image star5;
+    uint stars;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        time_valor = 00;
+        hour = 00;
+        day_valor = 1;
+        InvokeRepeating("IncrementTime", 0.0f, 0.2f);
+        star_points = 1000;
+        points_valor = 0;
+        stars = 0;
+        star1.sprite = Resources.Load<Sprite>("Sprites/star_black");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Points())
+            Stars();
+    }
+
+    void IncrementTime()
+    {
+        time_valor++;
+
+        if (time_valor > 59)
+        {
+            time_valor = 00;
+            hour += 1;
+        }
+
+        if (hour > 23)
+        {
+            day_valor += 1;
+            hour = 00;
+        }
+
+        day.text = day_valor.ToString();
+
+        if (hour < 10)
+            time.text = "0";
+        time.text += hour.ToString() + ":";
+
+        if (time_valor < 10)
+            time.text += "0";
+        time.text += time_valor.ToString();
+    }
+
+    bool Points()
+    {
+        bool ret = false;
+
+        if (star_points - points_valor == 0)
+            return true;
+
+        next_star.text = (star_points - points_valor).ToString();
+
+        return ret;
+    }
+
+    void Stars()
+    {
+        stars++;
+        star_points += star_points;
+
+        switch (stars)
+        {
+            case 1:
+                star1.sprite = Resources.Load<Sprite>("Sprites/star");
+                break;
+            default:
+                break;
+        }
+            
+
     }
 }
---
+++ FuckThePolice/Assets/Scripts/Turn_Number.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turn_Number : MonoBehaviour
+{
+    public GameObject turn_number_ui;
+    public TextMesh text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(turn_number_ui);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(turn_number_ui)
+        {
+            ShowTurn();
+        }
+    }
+
+    public void ShowTurn()
+    {
+        turn_number_ui.transform.position = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y + 4, this.gameObject.transform.position.z);
+    }
+    public void SetNumber(uint number)
+    {
+        //turn_number_ui.GetComponent<TextMesh>
+    }
+}
