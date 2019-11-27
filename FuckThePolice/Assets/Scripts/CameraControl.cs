f0ed7e5ab702aa07e6649ae926c95799ab9e1600
CereVenteo <36203330+CereVenteo@users.noreply.github.com>
2019-11-27 14:57:43 +0100

    limits camera movement

--- FuckThePolice/Assets/Scripts/CameraControl.cs
+++ FuckThePolice/Assets/Scripts/CameraControl.cs
@@ -12,21 +12,22 @@ public class CameraControl : MonoBehaviour
     {
         float moveAmount = 10f;
         float edgeSize = 30f;
+        uint limit = 20;
         cameraFollowPosition = transform.position;
 
-        if (Input.mousePosition.x > Screen.width - edgeSize || Input.GetKey(KeyCode.D))
+        if ((Input.mousePosition.x > Screen.width - edgeSize || Input.GetKey(KeyCode.D)) && cameraFollowPosition.z > -limit)
         {
             cameraFollowPosition.z -= moveAmount * Time.deltaTime;
         }
-        if (Input.mousePosition.x < edgeSize || Input.GetKey(KeyCode.A))
+        if ((Input.mousePosition.x < edgeSize || Input.GetKey(KeyCode.A)) && cameraFollowPosition.z < limit)
         {
             cameraFollowPosition.z += moveAmount * Time.deltaTime;
         }
-        if (Input.mousePosition.y > Screen.height - edgeSize || Input.GetKey(KeyCode.W))
+        if ((Input.mousePosition.y > Screen.height - edgeSize || Input.GetKey(KeyCode.W)) && cameraFollowPosition.x < limit)
         {
             cameraFollowPosition.x += moveAmount * Time.deltaTime;
         }
-        if (Input.mousePosition.y <  edgeSize || Input.GetKey(KeyCode.S))
+        if ((Input.mousePosition.y <  edgeSize || Input.GetKey(KeyCode.S)) && cameraFollowPosition.x > -limit)
         {
             cameraFollowPosition.x -= moveAmount * Time.deltaTime;
         }
