ae3c4620a08425af931cb74d81c4dd9614864b84
CereVenteo <36203330+CereVenteo@users.noreply.github.com>
2019-11-28 16:32:53 +0100

    button bugfix

---
+++ FuckThePolice/Assets/Scripts/Menus/Button_Call_Police.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_Call_Police : MonoBehaviour
+{
+    public GameObject menu;
+
+    private void OnMouseUpAsButton()
+    {
+        CallNextCitizen();
+        menu.SetActive(false);
+        
+    }
+
+    void CallNextCitizen()
+    {
+
+    }
+}
---
+++ FuckThePolice/Assets/Scripts/Menus/Police_Menu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Police_Menu : MonoBehaviour
+{
+    public GameObject menu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        menu.SetActive(false);
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        if (menu.activeSelf)
+        {
+            menu.SetActive(false);
+        }
+        else
+        {
+            menu.SetActive(true);
+            menu.transform.position = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y + 4, this.gameObject.transform.position.z);
+        }
+    }
+
+    void OnMouseDown()
+    {
+        CallNextCitizen();
+    }
+
+    void CallNextCitizen()
+    {
+
+    }
+}
